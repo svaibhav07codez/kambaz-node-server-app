da19a83675a3fe4343659360e9752a8c8b386637
Vaibhav S <vaibhavsankaran24@gmail.com>
2025-04-02 22:40:06 -0400

    first commit

---
+++ Lab5/index.js
@@ -0,0 +1,7 @@
+import PathParameters from "./PathParameters.js";
+export default function Lab5(app) {
+    app.get("/lab5/welcome", (req, res) => {
+      res.send("Welcome to Lab 5");
+    });
+    PathParameters(app);
+  };  
